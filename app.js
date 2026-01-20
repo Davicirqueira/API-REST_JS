30762b2e006971d8695c14e8b74e5858552d518f
Davicirqueira <daviforver17@gmail.com>
2026-01-20 18:30:31 -0300

    learning

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Focus.");
+import express from "express";
+
+const servidor = express();
+
+servidor.get('/helloworld', (req, resp) => {
+
+    resp.send('Foco.');
+
+});
+
+// Parâmetro Rota //
+servidor.get('/calculadora/:n1/:n2', (req, resp) => {
+
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    resp.send(n1 + n2);
+
+});
+
+servidor.listen(5001, () => console.log(`Api conectada com sucesso.`))
